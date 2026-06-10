06e4e3099eaa3b8111f5cdc15e42f287602c4d5a
HARSHVARDHAN SALVE <salveharsh17@gmail.com>
2026-06-11 01:18:24 +0530

    fixed issue in frotend

--- src/WeatherApp.jsx
+++ src/WeatherApp.jsx
@@ -1,28 +1,27 @@
-import {useState} from 'react';
+import { useState } from 'react';
 import SearchBox from './SearchBox.jsx';
 import InfoBox from './InfoBox.jsx';
 
-export default function WeatherApp(){
-    const [weatherInfo,setWeatherInfo] = useState({
-         city : "Indore",
-        feelslike:24.84,
-        temp :25 ,
-        tempMin :25 ,
-         tempMax : 25,
-         humidity : 47,
-         weather : "haze",
+export default function WeatherApp() {
+    const [weatherInfo, setWeatherInfo] = useState({
+        city: "Indore",
+        feelslike: 24.84,
+        temp: 25,
+        tempMin: 25,
+        tempMax: 25,
+        humidity: 47,
+        weather: "haze",
     });
 
     let updateInfo = (newInfo) => {
         setWeatherInfo(newInfo);
     };
 
-    return(
-<div>
-    <h1><i>Weather App</i></h1>
-    <SearchBox updateInfo={updateInfo}/>
-    <InfoBox info={weatherInfo}/>
-    
-</div>
+    return (
+        <div>
+            <h1>Weather App</h1>
+            <SearchBox updateInfo={updateInfo} />
+            <InfoBox info={weatherInfo} />
+        </div>
     );
 }
